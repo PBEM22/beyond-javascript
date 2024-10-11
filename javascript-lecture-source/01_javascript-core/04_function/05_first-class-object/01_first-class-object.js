ebddd95764ceca5dd392b322256eb71eea93b2df
blimu <sheep.ohgiraffers@gmail.com>
2024-10-11 11:08:41 +0900

    commit

---
+++ javascript-lecture-source/01_javascript-core/04_function/05_first-class-object/01_first-class-object.js
@@ -0,0 +1,34 @@
+// 일급 객체
+// 1. 무명의 리터럴로 생성할 수 있다. 즉, 런타임에 생성 가능하다.
+// 2. 변수나 자료구조(객체, 배열 등)에 저장할 수 있다.
+var hello = function() {
+    return '안녕!';
+}
+
+var obj = { hello };
+
+// 3. 함수의 매개변수로 전달할 수 있다.
+// 4. 함수의 반환 값으로 사용할 수 있다.
+function repeat(func, count) {
+    for(var i = 0; i < count; i++) {
+        console.log(func());
+    }
+
+    return function () {
+        console.log(`${count}번 반복 완료`);
+    }
+}
+
+var returnFunc = repeat(obj.hello, 10);
+returnFunc();
+
+
+
+
+
+
+
+
+
+
+
